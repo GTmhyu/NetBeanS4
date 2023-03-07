bb14dd3f89c7363d3f5f8bb9d828665830303470
UL <92898729+GTmhyu@users.noreply.github.com>
2023-03-07 15:08:30 +0700

    Add files via upload

---
+++ p01-Generic/src/main/java/com/mycompany/p01/generic/P01Generic.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p01.generic;
+
+/**
+ *
+ * @author Afkar
+ */
+public class P01Generic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
